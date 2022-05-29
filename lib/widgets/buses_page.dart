b01fb4077992c0fd7d8c3aae6ddaa5ae491e5a6d
OskierP <pakooskar@gmail.com>
2022-05-29 11:56:42 +0200

    time to next bus
    Added future builder to display time to next bus

--- lib/widgets/buses_page.dart
+++ lib/widgets/buses_page.dart
@@ -1,10 +1,6 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:tmc_lab/models/bus.dart';
 import 'package:tmc_lab/models/time_table.dart';
-
 import 'package:tmc_lab/services/api_service.dart';
 
 import 'package:tmc_lab/widgets/timetable_page.dart';
@@ -21,85 +17,154 @@ class BusesPage extends StatefulWidget {
 }
 
 class _BusesPage extends State<BusesPage> {
-  List<Container> _getBuses(var busId, var busNr, var buses) {
-    List<Container> list = [];
+  Column _getBuses(var busId, var busNr, var buses) {
+    List<SizedBox> list = [];
     for (Bus bus in buses) {
-      list.add(Container(
+      list.add(SizedBox(
         width: MediaQuery.of(context).size.width,
         height: 40,
-        child: TextButton(
-          child: Text(
-            bus.linia,
-            style: TextStyle(fontSize: 35, color: Colors.black),
-          ),
-          onPressed: () async {
-            List timetables = await ApiService.I
-                .getTimetable(widget.busstopId, widget.busstopNr, bus.linia);
-            Navigator.of(context).push(MaterialPageRoute(
-                builder: (context) =>
-                    TimetablePage(widget.info, bus.linia, timetables)));
+        child: FutureBuilder(
+          future: ApiService.I
+              .getTimetable(widget.busstopId, widget.busstopNr, bus.linia),
+          builder: (BuildContext context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            } else if (snapshot.hasData) {
+              return TextButton(
+                child: Text(
+                  '${bus.linia} - next bus in: ${timeToNextBus(bus, snapshot.data)}',
+                  style: TextStyle(
+                    fontSize: 35,
+                    color: timeColor(timeToNextBus(bus, snapshot.data))
+                        ? Colors.red
+                        : Colors.black,
+                  ),
+                ),
+                onPressed: () async {
+                  List timetables = await ApiService.I.getTimetable(
+                      widget.busstopId, widget.busstopNr, bus.linia);
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) =>
+                          TimetablePage(widget.info, bus.linia, timetables)));
+                },
+              );
+            } else {
+              return const Material(
+                  child: Text(
+                'Loading',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                ),
+                textAlign: TextAlign.center,
+              ));
+            }
           },
         ),
       ));
     }
 
-    return list;
+    return Column(
+      children: list,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: [
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(50),
-                        topRight: Radius.circular(50)),
-                    color: Colors.cyan),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      child: Container(
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Icon(
-                            Icons.clear,
-                            size: 50,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      alignment: Alignment.centerLeft,
+    return Column(
+      children: [
+        Flexible(
+          child: Container(
+            color: Colors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Icon(
+                      Icons.clear,
+                      size: 50,
+                      color: Colors.white,
                     ),
-                    Material(
-                      child: Text(
-                        '${widget.info.nazwa_zespolu.toString().toUpperCase()} ${widget.info.slupek}',
-                        style: TextStyle(fontSize: 35, color: Colors.white,backgroundColor: Colors.cyan),
-                      ),
+                  ),
+                  alignment: Alignment.centerLeft,
+                ),
+                Material(
+                  child: Text(
+                    '${widget.info.nazwa_zespolu.toString().toUpperCase()} ${widget.info.slupek}',
+                    style: const TextStyle(
+                      fontSize: 30,
+                      color: Colors.white,
                     ),
-                    Container()
-                  ],
+                  ),
+                  color: Colors.transparent,
                 ),
-              ),
-              flex: 1,
+                Container()
+              ],
             ),
-            Flexible(
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Column(
-                  children: _getBuses(
-                      widget.busstopId, widget.busstopNr, widget.buses),
-                ),
-              ),
-              flex: 8,
-            ),
-          ],
-        ));
+          ),
+          flex: 1,
+        ),
+        Flexible(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: _getBuses(widget.busstopId, widget.busstopNr, widget.buses),
+          ),
+          flex: 9,
+        ),
+      ],
+    );
+  }
+
+  String timeToNextBus(Bus bus, var timetables) {
+    String string = 'Not available';
+
+    for (Timetable time in timetables) {
+      if (compareTime(time.czas, DateTime.now())) {
+        print(bus.linia);
+
+        string = timeCalc(time.czas);
+        print(string);
+        break;
+      }
+    }
+    return string;
+  }
+
+  String timeCalc(var time) {
+    int min = int.parse(time.toString().substring(3, 5));
+    int hour = int.parse(time.toString().substring(0, 2));
+    print('$time');
+    print('hour: ${hour}');
+    print('min: ${min}');
+
+    if ((min - DateTime.now().minute) < 0) {
+      min = (60 + min) - DateTime.now().minute;
+      hour = (hour - 1) - DateTime.now().hour;
+    } else {
+      hour = hour - DateTime.now().hour;
+      min = min.toString().compareTo('00') == 0
+          ? 60 - DateTime.now().minute
+          : min - DateTime.now().minute;
+    }
+
+    String sHour = hour.toString().padLeft(2, '0');
+    String sMin = min.toString().padLeft(2, '0');
+
+    return '${sHour}h ${sMin}min';
+  }
+
+  bool timeColor(String time) {
+    if (time == 'Not available') {
+      return false;
+    }
+
+    int hour = int.parse(time.substring(0, 2));
+    int min = int.parse(time.substring(4, 6));
+
+    return (hour < 1 && min <= 5) ? true : false;
   }
 }
--- lib/widgets/timetable_page.dart
+++ lib/widgets/timetable_page.dart
@@ -1,49 +1,36 @@
-import 'dart:convert';
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:tmc_lab/models/bus.dart';
 import 'package:tmc_lab/models/time_table.dart';
-
-import 'package:tmc_lab/services/api_location.dart';
-
-import 'package:tmc_lab/widgets/location_page.dart';
-import 'package:http/http.dart' as http;
 
 class TimetablePage extends StatefulWidget {
   var info, times, bus;
 
   TimetablePage(this.info, this.bus, this.times);
 
+  DateTime dateTime = DateTime.now();
+
   @override
   State<StatefulWidget> createState() => _TimetablePage();
 }
 
 class _TimetablePage extends State<TimetablePage> {
-  List<Container> _getTime(var times) {
-    List<Container> list = [];
+  List<SizedBox> _getTime(var times) {
+    List<SizedBox> list = [];
 
     for (Timetable time in times) {
-      list.add(Container(
-        width: 200,
-        height: 40,
-        child: TextButton(
-          child: Text(
-            '${time.czas}',
-            style: TextStyle(fontSize: 25, color: Colors.black),
+      if (compareTime(time.czas, widget.dateTime)) {
+        list.add(SizedBox(
+          width: 200,
+          height: 40,
+          child: TextButton(
+            child: Text(
+              '${time.czas}',
+              style: const TextStyle(fontSize: 25, color: Colors.black),
+            ),
+            onPressed: () async {
+            },
           ),
-          onPressed: () async {
-            //print(time.brygada);
-            //List locations = await ApiLocation.I.getLocations(widget.bus, time.brygada);
-//             var response = await http.post(Uri.parse("https://api.um.warszawa.pl/api/action/busestrams_get/?resource_id=%20f2e5503e-927d-4ad3-9500-4ab9e55deb59&apikey=cf2be4bb-f716-4b02-a127-11003dd06008&type=1&line=169&brigade=1"));
-// print(response.body);
-            // Navigator.of(context).push(MaterialPageRoute(
-            //     builder: (context) =>
-            //         LocationPage(locations, widget.bus, widget.info)));
-          },
-        ),
-      ));
+        ));
+      }
     }
 
     return list;
@@ -52,31 +39,26 @@ class _TimetablePage extends State<TimetablePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         child: Column(
           children: [
             Flexible(
               child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.cyan,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50)),
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
                 ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      child: Container(
-                        child: TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: Icon(
-                            Icons.arrow_back_rounded,
-                            size: 50,
-                            color: Colors.white,
-                          ),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Icon(
+                          Icons.arrow_back_rounded,
+                          size: 50,
+                          color: Colors.white,
                         ),
                       ),
                       alignment: Alignment.centerLeft,
@@ -84,11 +66,12 @@ class _TimetablePage extends State<TimetablePage> {
                     Material(
                       child: Text(
                         '${widget.info.nazwa_zespolu.toString().toUpperCase()} ${widget.info.slupek}: BUS NR.${widget.bus}',
-                        style: TextStyle(
-                            fontSize: 35,
-                            color: Colors.white,
-                            backgroundColor: Colors.cyan),
+                        style: const TextStyle(
+                          fontSize: 35,
+                          color: Colors.white,
+                        ),
                       ),
+                      color: Colors.transparent,
                     ),
                     Container()
                   ],
@@ -97,16 +80,26 @@ class _TimetablePage extends State<TimetablePage> {
               flex: 1,
             ),
             Flexible(
-              child: Container(
-                child: Wrap(
-                  alignment: WrapAlignment.spaceAround,
-                  direction: Axis.vertical,
-                  children: _getTime(widget.times),
-                ),
+              child: Wrap(
+                alignment: WrapAlignment.spaceAround,
+                direction: Axis.vertical,
+                children: _getTime(widget.times),
               ),
               flex: 8,
             ),
           ],
         ));
   }
+}
+
+bool compareTime(var time1, DateTime time2) {
+  int hour1 = int.parse(time1.toString().substring(0, 2));
+  int minutes1 = int.parse(time1.toString().substring(3, 5));
+
+  bool ret = ((hour1 >= time2.hour && minutes1 >= time2.minute) ||
+          (hour1 > time2.hour))
+      ? true
+      : false;
+
+  return ret;
 }
